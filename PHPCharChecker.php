3f8a27e62af368ef5080cd7921059db1031f1ba8
Sebastian <65468884+dynamiccodePL@users.noreply.github.com>
2020-05-17 01:16:50 +0200

    Create PHPCharChecker.php

---
+++ PHPCharChecker.php
@@ -0,0 +1,27 @@
+<?php
+
+$string = 'Czy to zdanie jest zakończone kropką?';
+
+function char_checker($char, $string = '') {
+
+if($string === '') die('Podaj jakiś tekst.');
+
+$words = explode(' ', $string);
+
+$last_word = end($words);
+
+$last_char = strlen($last_word) - 1;
+
+if($last_word[$last_char] === $char) {
+
+echo "Find search char '" . $char . " in the end string.'";
+
+}
+
+else {
+
+echo "NOT found char '" . $char . "' in the end string.";
+
+}
+
+}
